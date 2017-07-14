3887fe0b6d1a6c700883f781ccc1e2658f32ae1d
Jade Anderson <enighter@gmail.com>
2017-07-14 02:58:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(int)
+  if int < 2
+    return false
+  elsif int < 4
+    return true
+  elsif int.even? or int % 3 == 0
+    return false
+  else
+    n = 5
+    while n * n <= int
+      if int % n == 0 || int % (n + 2) == 0
+        return false
+      end
+      n += 6
+    end
+    true
+  end
+end
